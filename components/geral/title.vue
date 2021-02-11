619ea292c5f92e246f47e09764c6a636d9d8b0de
DivaUaci <diva.uaciquete@gmail.com>
2021-02-12 02:09:13 +0800

    first commit

---
+++ components/geral/title.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-card
+    class="mx-auto"
+    max-width="344"
+    >
+  
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="overline mb-4">
+            OLA {{ nome }}
+        </div>
+        <v-list-item-title class="headline mb-1">
+          Welcome to Vuetify
+        </v-list-item-title>
+        <v-list-item-subtitle>a basic lesson for the use of Vuetify</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+      </v-list-item>
+      </v-card>
+          
+</template>
+
+<script lang='ts'>
+import {Vue, Component, Emit, Prop} from 'nuxt-property-decorator'
+
+@Component
+export default class Title extends Vue {
+    @Prop() nome! : string
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template lang='pug'>
+    Title(:nome="nomeCompleto")
+</template>
+
+<script lang='ts'>
+import {Component, Vue} from 'nuxt-property-decorator'
+import Title from '../components/geral/title.vue'
+
+@Component({
+components: {
+  Title
+}
+})
+export default class extends Vue{
+  nomeCompleto = "Diva Flora"
+}
+</script>
+
+<style>
+.myStyle {
+  height: 500px;
+  width: 300px;
+  border: 1px solid red;
+  margin: 0 auto;
+}
+</style>
